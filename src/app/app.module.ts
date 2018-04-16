e5e098a27b44203b4d457b041e04c920b47c02f2
David Paz <david.paz@intellishop.ag>
2018-04-16 13:31:15 +0200

    Provide connection through angular di
    Delegate the creation of the connection to the factory function. Allow
    to inject the connection into components through @Inject().
    
    Changes:
    * Add provider for Connection with needed dependencies
    * Inject connection into app.component
    * Only connect from app.component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,78 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {
+    IonicApp, IonicErrorHandler, IonicModule, Platform
+} from 'ionic-angular';
 
-import { MyApp } from './app.component';
+import { Connection, getConnectionManager } from 'typeorm'
 import { HomePage } from '../pages/home/home';
 
+import { MyApp } from './app.component';
+import { Author } from '../entities/author';
+import { Category } from '../entities/category';
+import { Post } from '../entities/post';
+
+function getOrmConnection(platform: Platform): Connection {
+    if (platform.is('cordova')) {
+        // Running on device or emulator
+        return getConnectionManager()
+            .create({
+                type: 'cordova',
+                database: 'test',
+                location: 'default',
+                logging: ['error', 'query', 'schema'],
+                synchronize: true,
+                entities: [
+                    Author,
+                    Category,
+                    Post
+                ]
+            });
+    } else {
+        // Running app in browser
+        return getConnectionManager()
+            .create({
+                type: 'sqljs',
+                autoSave: true,
+                location: 'browser',
+                logging: ['error', 'query', 'schema'],
+                synchronize: true,
+                entities: [
+                    Author,
+                    Category,
+                    Post
+                ]
+            });
+    }
+}
+
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        {
+            provide: 'TypeOrmConnection',
+            useFactory: getOrmConnection,
+            deps: [Platform]
+        }
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
